1035d23a57c5420af7548eb07532d45b86e776de
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-11-20 17:27:21 +0100

    Update autoassegna.php

--- Class/autoassegna.php
+++ Class/autoassegna.php
@@ -5,12 +5,29 @@ Php Tested: 5.6.31
 */
 if(defined('ABSPATH')){
  class xx{
- 
+
 	function __construct(){
+	
+ /*	
 		for($i=0,$m=func_get_args(),$l=count($m),$o=&$this,$p=['name'];$i<$l;++$i){$o->$p[$i]=$m[$i];};
 		unset($i,$m,$l,$p);
+*/	
+	$m=func_get_args();
+	$l=count($m);
+	$i=0;
+	$n=[
+	 'type_name_plural ',
+	 'type_name_singular',
+	];
+	$o=&$this;
+	while($i<$l){$o[$m[$i]]=$n[$i];++$i;};
+	unset($i,$l,$m,$n);		
+		
 	}
 
+	 
+ 
+	 
 
    function __destruct(){
 	$o=&$this;
