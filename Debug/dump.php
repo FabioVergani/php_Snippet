2c3d0c2c7453645c1365bcdd27718693027e2b61
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-11-24 15:46:03 +0100

    Update dump.php

--- Debug/dump.php
+++ Debug/dump.php
@@ -1,20 +1,3 @@
-	 static private function dump(){  
-		$m=func_get_args();
-		$l=count($m);
-		$i=0;
-		$n=null;
-		$a=['<','>'];
-		$b=['&lt;','&gt;'];
-		$e=PHP_EOL;
-		ob_start();
-		echo '<pre class="dump n',$i,'">';
-		while($i<$l){echo $e,empty($n=$m[$i])?'Empty':str_replace($a,$b,print_r($n,true)).$e.'<hr />';++$i;};
-		$n=ob_get_clean();
-		echo $n,$e,'</pre>',$e;
-	 }
-	 
-
-function dump(){$m=func_get_args();$l=count($m);$i=0;$n=null;$a=['<','>'];$b=['&lt;','&gt;'];$e=PHP_EOL;ob_start();echo '<pre class="dump n',$i,'">';while($i<$l){echo $e,empty($n=$m[$i])?'Empty':str_replace($a,$b,print_r($n,true)).'<hr />';++$i;};$n=ob_get_clean();echo $n,'</pre>',$e;}
-
-	 
+function dump(){$m=func_get_args();$l=count($m);$i=0;$n=null;$a=['<','>'];$b=['&lt;','&gt;'];$e=PHP_EOL;ob_start();echo '<pre class="dump n',$i,'">';while($i<$l){echo $e,empty($n=$m[$i])?'Empty':str_replace($a,$b,print_r($n,true)),$e,'<hr />';++$i;};$n=ob_get_clean();echo $n,$e,'</pre>',$e;}
+ 
 	 
