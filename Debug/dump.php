7aa5400e986a191b3935e253d53f7f3541756420
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-11-24 15:42:15 +0100

    Update dump.php

--- Debug/dump.php
+++ Debug/dump.php
@@ -8,7 +8,13 @@
 		$e=PHP_EOL;
 		ob_start();
 		echo '<pre class="dump n',$i,'">';
-		while($i<$l){echo empty($n=$m[$i])?'Empty':str_replace($a,$b,print_r($n,true)).'<hr />';++$i;};
+		while($i<$l){echo $e,empty($n=$m[$i])?'Empty':str_replace($a,$b,print_r($n,true)).'<hr />';++$i;};
 		$n=ob_get_clean();
 		echo $n,'</pre>',$e;
 	 }
+	 
+
+function dump(){$m=func_get_args();$l=count($m);$i=0;$n=null;$a=['<','>'];$b=['&lt;','&gt;'];$e=PHP_EOL;ob_start();echo '<pre class="dump n',$i,'">';while($i<$l){echo $e,empty($n=$m[$i])?'Empty':str_replace($a,$b,print_r($n,true)).'<hr />';++$i;};$n=ob_get_clean();echo $n,'</pre>',$e;}
+
+	 
+	 
