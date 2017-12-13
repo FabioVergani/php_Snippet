79177be53a8530122ebef00eed6cf5b4246f64e8
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-12-13 17:26:02 +0100

    Create full_url.php

---
+++ Url/full_url.php
@@ -0,0 +1,9 @@
+		function full_url($s){
+			$ssl=(!empty($s['HTTPS']) && $s['HTTPS']=='on')? true:false;
+			$sp=strtolower($s['SERVER_PROTOCOL']);
+			$protocol=substr($sp,0,strpos($sp,'/')).(($ssl)?'s':'');
+			$port=$s['SERVER_PORT'];
+			$port=((!$ssl && $port=='80')||($ssl && $port=='443'))?'':':'.$port;
+			$host=isset($s['HTTP_HOST'])? $s['HTTP_HOST'] : $s['SERVER_NAME'];
+			return $protocol.'://'.$host.$port.$s['REQUEST_URI'];
+		}
