bda64aea2e4e2ac2b9e9478c0e751f896a525116
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-02-16 11:33:23 +0100

    Update WeekNumber.php

--- Date/WeekNumber.php
+++ Date/WeekNumber.php
@@ -6,6 +6,14 @@ echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 */
 $DateYmd=date('Y-m-d');
-$WeekNumber=date('W',strtotime($DateYmd));
-echo 'DateYmd: ',$DateYmd,PHP_EOL,'WeekNumber: ',$WeekNumber;
+$Time=strtotime($DateYmd);
+$Year=date('Y',$Time);
+$WeekNumber=date('W',$Time);
+
+echo '<table><tbody>';
+echo '<tr><td>Time:</td><td>',$Time,'</td></tr>';
+echo '<tr><td>Day:</td><td>',$DateYmd,'</td></tr>';
+echo '<tr><td>Year:</td><td>',$Year,'</td></tr>';
+echo '<tr><td>Week:</td><td>',$WeekNumber,'</td></tr>';
+echo '</tbody></table>';
 ?>    
